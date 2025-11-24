7f3f9cc1d4a48e987119d008c002dafec005394f
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2025-11-25 00:38:12 +0300

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,11 @@
+# Input settings
+INPUT_CHANNELS = 3       # RGB
+INPUT_SIZE = (299, 299) # Xception
+
+# Model settings
+NUM_CLASSES = 1000      # ImageNet default
+REPETITIONS_MIDDLE = 8  # Middle flow repeat
+
+# Training settings 
+LEARNING_RATE = 0.001
+BATCH_SIZE = 32
